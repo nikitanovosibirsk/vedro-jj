03ae727a9b664a93af75c51c42116286474db1a7
Nikita <nikitanovosibirsk@yandex.com>
2022-06-09 22:20:07 +0300

    add threaded (#2)

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,8 +1,60 @@
-from vedro.core import Plugin
+import jj
+import pytest
+from aiohttp import ClientSession
+from jj.mock import Mocked, mocked
+from vedro.core import Dispatcher, Report
+from vedro.events import CleanupEvent, StartupEvent
 
-from vedro_jj import RemoteMock, RemoteMockPlugin
+import vedro_jj
+from vedro_jj import RemoteMockPlugin
 
 
-def test_interactive_plugin():
+@pytest.fixture()
+def dispatcher() -> Dispatcher:
+    return Dispatcher()
+
+
+@pytest.fixture()
+async def plugin(dispatcher: Dispatcher) -> RemoteMockPlugin:
+    class RemoteMock(vedro_jj.RemoteMock):
+        threaded = False
+
     plugin = RemoteMockPlugin(RemoteMock)
-    assert isinstance(plugin, Plugin)
+    plugin.subscribe(dispatcher)
+
+    await dispatcher.fire(StartupEvent([]))
+    yield plugin
+    await dispatcher.fire(CleanupEvent(Report()))
+
+
+@pytest.fixture()
+async def plugin_threaded(dispatcher: Dispatcher) -> RemoteMockPlugin:
+    class RemoteMockThreaded(vedro_jj.RemoteMock):
+        threaded = True
+
+    plugin = RemoteMockPlugin(RemoteMockThreaded)
+    plugin.subscribe(dispatcher)
+
+    await dispatcher.fire(StartupEvent([]))
+    yield plugin
+    await dispatcher.fire(CleanupEvent(Report()))
+
+
+@pytest.fixture()
+async def mock() -> Mocked:
+    async with mocked(jj.match("*"), jj.Response()) as mock:
+        yield mock
+
+
+@pytest.mark.asyncio
+async def test_mock(plugin: RemoteMockPlugin, mock: Mocked):
+    async with ClientSession() as session:
+        response = await session.get("http://localhost:8080")
+    assert response.status == 200
+
+
+@pytest.mark.asyncio
+async def test_mock_threaded(plugin_threaded: RemoteMockPlugin, mock: Mocked):
+    async with ClientSession() as session:
+        response = await session.get("http://localhost:8080")
+    assert response.status == 200
--- vedro_jj/_remote_mock.py
+++ vedro_jj/_remote_mock.py
@@ -1,6 +1,8 @@
 import asyncio
+from asyncio import AbstractEventLoop, Task
 from functools import partial
-from typing import Type, Union
+from threading import Thread
+from typing import Any, Type, Union
 
 from aiohttp import web
 from jj import default_app, default_handler
@@ -22,28 +24,55 @@ class _Mock(Mock):
 class RemoteMockPlugin(Plugin):
     def __init__(self, config: Type["RemoteMock"]) -> None:
         super().__init__(config)
-        self._server: Union[Server, None] = None
         self._host = config.host
         self._port = config.port
+        self._threaded = config.threaded
+        self._server: Union[Server, None] = None
+        self._task: Union["Task[Any]", None] = None
+        self._thread: Union[Thread, None] = None
 
     def subscribe(self, dispatcher: Dispatcher) -> None:
         dispatcher.listen(StartupEvent, self.on_startup) \
                   .listen(CleanupEvent, self.on_cleanup)
 
-    def _create_server(self) -> Server:
+    def _create_server(self, loop: AbstractEventLoop) -> Server:
         resolver = ReversedResolver(Registry(), default_app, default_handler)
         runner = partial(AppRunner, resolver=resolver, middlewares=[SelfMiddleware(resolver)])
-        return Server(asyncio.get_event_loop(), runner, web.TCPSite)  # type: ignore
+        return Server(loop, runner, web.TCPSite)  # type: ignore
 
-    async def on_startup(self, event: StartupEvent) -> None:
-        self._server = self._create_server()
+    def _serve_mock_threaded(self) -> None:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        self._server = self._create_server(loop)
         self._server.start(_Mock(), self._host, self._port)
+        try:
+            self._server.serve()
+        finally:
+            self._server.shutdown()
+
+        asyncio.set_event_loop(None)
+
+    def on_startup(self, event: StartupEvent) -> None:
+        if self._threaded:
+            self._thread = Thread(target=self._serve_mock_threaded, daemon=True)
+            self._thread.start()
+        else:
+            self._server = self._create_server(asyncio.get_event_loop())
+            self._server.start(_Mock(), self._host, self._port)
+            self._task = asyncio.create_task(self._server._serve())
 
     async def on_cleanup(self, event: CleanupEvent) -> None:
-        if self._server is None:
-            return
-        for runner in reversed(self._server._runners):
-            await runner.cleanup()
+        if self._server:
+            self._server.cleanup()
+
+        if self._thread:
+            self._thread.join(1.0)
+            self._thread = None
+
+        if self._task:
+            await self._task
+            self._task = None
 
 
 class RemoteMock(PluginConfig):
@@ -51,3 +80,7 @@ class RemoteMock(PluginConfig):
 
     host: str = "0.0.0.0"
     port: int = 8080
+
+    # Start mock in a separate thread
+    # Mandatory if blocking calls are used
+    threaded: bool = False
